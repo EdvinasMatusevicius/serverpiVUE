b4ed171bab76483a0621844d686a8cf256d4d5ed
Edvinas <edvinas2121@hotmail.com>
2020-07-16 02:03:19 +0300

    started adding shell controls

--- src/views/Shell.vue
+++ src/views/Shell.vue
@@ -1,8 +1,22 @@
 <template>
     <div class="shell-panel">
-        <v-btn class="controlls">
-            Coposer install
-        </v-btn>
+        <v-expansion-panels class="controlls">
+           <v-expansion-panel  v-for="group of controlls" v-bind:key="group.id" class="controlls__panel">
+             <v-expansion-panel-header>{{group.groupName}}</v-expansion-panel-header>
+             <v-expansion-panel-content>
+
+                 <div v-for="button of group.buttons" :key="button.id" class="controlls__btn d-inline-flex justify-center">
+                    <div class="align-self-center">{{button.name}}</div>
+                    <v-spacer></v-spacer>
+                    <v-btn outlined fab :color="button.color" class="align-self-center">
+                        <v-icon>{{button.icon}}</v-icon>
+                    </v-btn>
+                </div>
+
+             </v-expansion-panel-content>
+           </v-expansion-panel>
+           
+         </v-expansion-panels>
         <shell-output/>
     </div>
 </template>
@@ -13,6 +27,74 @@ export default {
     name:'shell-panel',
     components:{
         ShellOutput
+    },
+    data(){
+        return{
+            controlls:[
+                {
+                    id:1,
+                    groupName:'Package installation',
+                    buttons:[
+                        {
+                            id:1,
+                            name:'composer install',
+                            icon:'mdi-package-down',
+                            color:'#4592af',
+                        },
+                        {
+                            id:2,
+                            name:'npm install',
+                            icon:'mdi-package-down',
+                            color:'#34b378',
+                        }
+                    ]
+                },
+                {
+                    id:2,
+                    groupName:'Env configuration',
+                    buttons:[
+                        {
+                            id:1,
+                            name:'Create and copy env values from env.example',
+                            icon:'mdi-file-plus',
+                            color:'#3f3f3fb6',
+                        },
+                        {
+                            id:2,
+                            name:'Create empty env file',
+                            icon:'mdi-file-plus-outline',
+                            color:'#3f3f3fb6',
+                        },
+                        {
+                            id:3,
+                            name:'Generate app key',
+                            icon:'mdi-key-plus',
+                            color:'#4592af',
+                        },
+                        {
+                            id:4,
+                            name:'Link storage (filesystem driver must be set if not default)',
+                            icon:'mdi-link-variant-plus',
+                            color:'#4592af',
+                        },
+                        {
+                            id:5,
+                            name:'Get env file values',
+                            icon:'mdi-file-download-outline',
+                            color:'#3f3f3fb6',
+                        },
+                         {
+                            id:6,
+                            name:'Save env vals (below text area)',
+                            icon:'mdi-file-upload',
+                            color:'#3f3f3fb6',
+                        }
+                    ]
+                },
+            ]
+
+            
+        }
     }
 }
 </script>
@@ -24,20 +106,27 @@ export default {
         margin:0 auto;
         display:grid;
         grid-template-columns: 4fr 10fr;
-        grid-template-rows: auto;
+        grid-template-rows: auto auto;
         grid-template-areas:
-        "controll output"
+        "control output"
+        ". output"
     }
     .controlls{
         min-width: 15rem;
-        grid-area: controll;
+        height:auto;
+        grid-area: control;
+
+        &__btn{
+            width: 100%;
+            border:1px solid beige;
+        }
     }
   @media (max-width: 600px) {
       .shell-panel{
           grid-template-columns: 100%;
         grid-template-rows: auto auto;
              grid-template-areas:
-        "controll"
+        "control"
         " output"
       }
   }
